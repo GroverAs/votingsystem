f0b0c5d2e95b39241267a54ff95de6a7a6d87098
Alexander Savelev <alex.savelev21@yandex.ru>
2023-12-13 21:38:11 +0500

    Add data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,35 @@
-INSERT INTO USERS (name, email, password)
-VALUES ('User', 'user@yandex.ru', '{noop}password'),
+DELETE FROM users;
+DELETE FROM user_role;
+DELETE FROM dish;
+DELETE FROM vote;
+DELETE FROM restaurant;
+
+INSERT INTO users (name, email, password)
+VALUES ('User1', 'user1@yandex.ru', '{noop}password'),
        ('Admin', 'admin@gmail.com', '{noop}admin'),
-       ('Guest', 'guest@gmail.com', '{noop}guest');
+       ('User2', 'user2@yandex.ru', '{noop}password'),
+       ('User3', 'user3@yandex.ru', '{noop}password'),
+       ('User4', 'user4@yandex.ru', '{noop}password');
 
-INSERT INTO USER_ROLE (user_id, role)
+INSERT INTO user_role (role, user_id)
 VALUES ('USER', 1),
-       ('ADMIN', 2),
-       ('USER', 2);
+       ('ADMIN', 1),
+       ('USER', 2),
+       ('USER', 3),
+       ('USER', 4);
 
+INSERT INTO restaurant (name)
+VALUES ('Moscow time'),
+       ('Meat place'),
+       ('Yapona papa');
+
+INSERT INTO dish (name, price, restaurant_id)
+VALUES ('Pizza', 480, 1),
+       ('Steak', 890, 2),
+       ('Sushi set', 1299, 3);
+
+INSERT INTO vote (user_id, restaurant_id, vote_time)
+VALUES (1, 1,'2023-12-10'),
+       (2, 1,'2023-12-10'),
+       (3, 2,'2023-12-10'),
+       (4, 3,'2023-12-10');
--- src/main/resources/initDB.sql
+++ src/main/resources/initDB.sql
@@ -5,11 +5,11 @@ DROP TABLE IF EXISTS restaurant;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
 
-CREATE SEQUENCE global_seq START WITH 100000;
+-- CREATE SEQUENCE global_seq START WITH 100000;
 
 CREATE TABLE users
 (
-    id         INTEGER   DEFAULT nextval('global_seq') PRIMARY KEY,
+    id         INTEGER AUTO_INCREMENT PRIMARY KEY,
     name       VARCHAR                 NOT NULL,
     email      VARCHAR                 NOT NULL,
     password   VARCHAR                 NOT NULL,
@@ -28,29 +28,29 @@ CREATE TABLE user_role
 
 CREATE TABLE restaurant
 (
-    id   INTEGER DEFAULT nextval('global_seq') PRIMARY KEY,
+    id   INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR NOT NULL
 );
 CREATE UNIQUE INDEX restaurant_unique_name_idx ON restaurant (name);
 
 CREATE TABLE dish
 (
-    id            INTEGER DEFAULT nextval('global_seq') PRIMARY KEY,
-    name          VARCHAR               NOT NULL,
-    price         DOUBLE PRECISION      NOT NULL,
-    restaurant_id INTEGER               NOT NULL,
-    add_time      DATE    DEFAULT now() NOT NULL,
+    id            INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name          VARCHAR            NOT NULL,
+    price         DOUBLE PRECISION   NOT NULL,
+    restaurant_id INTEGER            NOT NULL,
+    add_time      DATE DEFAULT now() NOT NULL,
     FOREIGN KEY (restaurant_id) REFERENCES restaurant (id) ON DELETE CASCADE
 );
 CREATE UNIQUE INDEX dish_unique_restaurant_name_idx ON dish (name, add_time, restaurant_id);
 
 CREATE TABLE vote
 (
-    id            INTEGER DEFAULT nextval('global_seq') PRIMARY KEY,
+    id            INTEGER AUTO_INCREMENT PRIMARY KEY,
     user_id       INTEGER   NOT NULL,
     restaurant_id INTEGER   NOT NULL,
-    vote_date     TIMESTAMP NOT NULL,
+    vote_time     TIMESTAMP NOT NULL,
     FOREIGN KEY (restaurant_id) REFERENCES restaurant (id) ON DELETE CASCADE,
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
-CREATE UNIQUE INDEX vote_unique_date_idx ON vote (user_id, vote_date);
+CREATE UNIQUE INDEX vote_unique_date_idx ON vote (user_id, vote_time);
